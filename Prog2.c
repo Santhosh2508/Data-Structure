d3e7fd87fd81dc310ff8f5fe11f8784b1f479ba5
Santhosh2508 <90123284+Santhosh2508@users.noreply.github.com>
2022-02-14 19:44:31 +0530

    String
    Design, Develop and Implement a Program in C for the following operations on Strings.
    a. Read a main String (STR), a Pattern String (PAT) and a Replace String (REP)
    b. Perform Pattern Matching Operation: Find and Replace all occurrences of PAT in
    STR with REP if PAT exists in STR. Report suitable messages in case PAT does not
    exist in STR
    Support the program with functions for each of the above operations. Don't use Built-in
    functions.

---
+++ Prog2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+// Global Variable
+char STR[50], PAT[20], REP[20], ANS[100];
+
+// Find and Replace String Match
+void stringmatch()
+{
+    int i, m, c, j, k, flag = 0;
+    i = m = c = j = 0;
+    // iterates STR Characters
+    while (STR[c] != '\0')
+    {
+        // checking for match
+        if (STR[m] == PAT[i])
+        {
+            i++;
+            m++;
+            if (PAT[i] == '\0')
+            {
+                // replace string in ANS string
+                flag = 1;
+                for (k = 0; REP[k] != '\0'; k++, j++)
+                {
+                    ANS[j] = REP[k];
+                }
+                i = 0;
+                c = m;
+            }
+        }
+        else // mismatch
+        {
+            ANS[j] = STR[c];
+            j++;
+            c++;
+            m = c;
+            i = 0;
+        }
+    }
+    ANS[j] = '\0';
+    if (flag == 0)
+    {
+        printf("\n\nPattern does not found\n");
+    }
+    else
+    {
+        printf("\n\nTHE RESULTANT string is : %s\n", ANS);
+    }
+}
+
+// Main Fuction
+void main()
+{
+    printf("\nEnter the MAIN string : ");
+    gets(STR);
+    printf("\n\nEnter the PATTERN string: ");
+    gets(PAT);
+    printf("\n\nEnter the REPLACE string : ");
+    gets(REP);
+    stringmatch();
+}
